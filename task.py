dc51e0460d3813e627a69bcacbaa0106ee89aa11
jgall <gal88.jorge@gmail.com>
2025-04-12 17:50:32 -0400

    First commit

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band name Generator")
+city_name = input("What is your city name?\n")
+pet_name =  input("What is your dog name?\n")
+print("Your band name could be: " + city_name + " " + pet_name)
