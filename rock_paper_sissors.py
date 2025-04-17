85d23b7a95252c04ab177baf7974f208f6781df7
jgall <gal88.jorge@gmail.com>
2025-04-16 23:29:48 -0400

    Add fix to rock, paper, sissors rules

--- rock_paper_sissors.py
+++ rock_paper_sissors.py
@@ -31,6 +31,8 @@ sissors_ascii = ("""
 """)
 
 game_choices = ["Rock", "Paper", "Sissors"]
+image_choices = [rock_ascii, paper_ascii, sissors_ascii]
+
 computer_choice_index = random.randint(0, 2)
 my_choice = input("Please select an option R for rock, P for papper, S for Sissors:\n").upper()
 
@@ -40,34 +42,28 @@ if my_choice != "R" and my_choice != "P" and my_choice != "S":
 
 if my_choice == "R":
     my_choice_index = 0
-    print(rock_ascii)
 
 if my_choice == "P":
     my_choice_index = 1
-    print(paper_ascii)
 
 if my_choice == "S":
     my_choice_index = 2
-    print(sissors_ascii)
 
 print(f"You chose: {game_choices[my_choice_index]} \n")
+print(image_choices[my_choice_index])
 print("\n")
 print(f"Computer choice: {game_choices[computer_choice_index]} \n")
-
-if computer_choice_index == 0:
-    print(rock_ascii)
-
-if computer_choice_index == 1:
-    print(paper_ascii)
-
-if computer_choice_index == 2:
-    print(sissors_ascii)
-
+print(image_choices[computer_choice_index])
 print("\n")
 
 if my_choice_index == computer_choice_index:
     print("Draw")
+elif computer_choice_index == 2 and my_choice_index == 0:
+    print("You win!!! \U0001F606")
+    print(f"{game_choices[my_choice_index]} beats {game_choices[computer_choice_index]}")
 elif computer_choice_index > my_choice_index:
     print("Computer wins \U0001F610")
+    print(f"{game_choices[computer_choice_index]} beats {game_choices[my_choice_index]}")
 else:
     print("You win!!! \U0001F606")
+    print(f"{game_choices[my_choice_index]} beats {game_choices[computer_choice_index]}") 
