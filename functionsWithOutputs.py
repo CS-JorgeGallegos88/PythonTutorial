34bba3512214bc319469a850f132e136cc15902b
jgall <gal88.jorge@gmail.com>
2025-04-30 22:08:04 -0400

    Add IsLeap year excercise

--- functionsWithOutputs.py
+++ functionsWithOutputs.py
@@ -19,3 +19,16 @@ def format_name(f_name:str, l_name:str):
 
 print(format_name("jorge", "gallegos"))
 
+
+#Leap years
+def is_leap_year(year):
+    isLeap = False
+    if year % 4 == 0:
+        if year % 100 > 0:
+            return True
+        else: 
+            if year % 400 == 0:
+                return True
+    return isLeap
+
+print(is_leap_year(2020))
